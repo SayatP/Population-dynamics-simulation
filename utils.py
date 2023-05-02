9e65e0842918c15d946e69a690386e3a817acc6f
Sayat Petrosyan <v.sayat.petrosyan@reddit.com>
2023-05-02 14:15:16 +0400

    fix collisions

--- utils.py
+++ utils.py
@@ -10,38 +10,68 @@ def two_int_to_hash(i1, i2):
 def numpy_arr_to_str(arr):
     return '|'.join(''.join(str(i) for i in row) for row in arr)
 
-def get_single_value(t, agent_coords):
+def manhattan_distance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+def closest_number_2(array, x1, y1):
+    target_value = 2
+    closest_points = []
+    min_distance = float('inf')
+
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            if array[i, j] == target_value:
+                distance = manhattan_distance((i, j), (x1, y1))
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_points = [(i, j)]
+                elif distance == min_distance:
+                    closest_points.append((i,j))
+
+    return closest_points
+
+
+def get_single_value(matrix, agent_coords):
         distances = dict()
-        for x in range(t.shape[0]):
-            for y in range(t.shape[1]):
+        for x in range(matrix.shape[0]):
+            for y in range(matrix.shape[1]):
                 if x != agent_coords[0] or y != agent_coords[1]:
                     dist = abs(agent_coords[0]-x) + abs(agent_coords[1]-y)
 
                     if dist not in distances:
-                        distances[dist] = [t[x,y].item()]
+                        distances[dist] = [(x, y, matrix[x,y].item())]   # x-coord, y-coord, value
                     else:
-                        distances[dist].append(t[x,y].item())
+                        distances[dist].append((x, y, matrix[x,y].item()))
+
+
+        terminal = 0
 
         for i, key in enumerate(sorted(distances.keys())):
             item = distances[key]
 
-            if i == 0 and 1 in item:
+            if i == 0 and 1 in [triplet[2] for triplet in item]:
                 return 1
             
-            if 1 in item or 2 in item:
-                if item.count(2) > item.count(1):
-                    return 2
-                return 1
+            for triplet in item:
+                if triplet[2] == 2:
+                    terminal = 2
 
-        return 0
+                if triplet[2] == 1:
+                    if tuple(agent_coords) in closest_number_2(matrix, triplet[0], triplet[1]):
+                        return 1
 
-def approximate_5x5_to_3x3(t):
+        return terminal
+
+
+def approximate_5x5_to_3x3(matrix):
     new_t = np.zeros((3,3), dtype=np.int64)
     new_t[1,1] = 2
 
     for i in range(3):
         for j in range(3):
             if i != 1 or j != 1:
-                new_t[i,j] = get_single_value(t[i:i+3,j:j+3], (2-i,2-j))
+                new_t[i,j] = get_single_value(matrix[i:i+3,j:j+3], (2-i,2-j))
 
     return new_t
+
